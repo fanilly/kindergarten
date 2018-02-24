c1300d57a0bf673f53551866ed71d372a76654a0
fanilly <815969707@qq.com>
2018-02-24 17:52:52 +0800

    添加我的页面

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,66 +1,40 @@
 // pages/me/me.js
+const app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    userInfo:app.globalData.userInfo,
+    btns:[{
+      name:'已接单',
+      value:99,
+      url:'/'
+    },{
+      name:'教练等级',
+      value:'III',
+      url:'/'
+    },{
+      name:'诚信度',
+      value:66,
+      url:'/'
+    },{
+      name:'收益',
+      value:99785,
+      url:'/'
+    }]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // 生命周期函数--监听页面加载
   onLoad: function (options) {
   
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  //生命周期函数--监听页面初次渲染完成
   onReady: function () {
   
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  // 生命周期函数--监听页面显示
   onShow: function () {
   
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
-})
+});
