14c056de57c9b74ad2c2b6fdddd58557ca4fe782
fanilly <815969707@qq.com>
2018-02-26 12:01:12 +0800

    添加历史订单详情页面

---
+++ pages/orderHistoryDetail/orderHistoryDetail.js
@@ -0,0 +1,21 @@
+// pages/orderHistoryDetail/orderHistoryDetail.js
+Page({
+  data: {
+    btns: [{
+      name: '一级教练',
+      value: '10人'
+    }, {
+      name: '二级教练',
+      value: '3人'
+    }, {
+      name: '三级教练',
+      value: '1人'
+    }]
+  },
+
+  // 生命周期函数--监听页面加载
+  onLoad: function(options) {
+
+  }
+
+});
