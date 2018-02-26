c78082738c4e317fe49bc88ce90d3bec61107d78
fanilly <815969707@qq.com>
2018-02-26 14:43:45 +0800

    添加实名认证页面

---
+++ pages/realNameCertification/realNameCertification.js
@@ -0,0 +1,12 @@
+// pages/realNameCertification/realNameCertification.js
+const app = getApp();
+Page({
+  data: {
+    userInfo:app.globalData.userInfo
+  },
+
+  // 生命周期函数--监听页面加载
+  onLoad(options) {
+
+  }
+});
